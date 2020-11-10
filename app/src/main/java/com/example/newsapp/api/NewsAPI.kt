74ebc26256055a4795fd326b2be6912073f97cc0
Sommie09 <ceekay2a7@gmail.com>
2020-11-10 13:10:52 +0100

    Set up Retofit

---
+++ app/src/main/java/com/example/newsapp/api/NewsAPI.kt
@@ -0,0 +1,42 @@
+package com.example.newsapp.api
+
+import com.example.newsapp.data.NewsResponse
+import com.example.newsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+
+        //These queries are gotten from the full get request string that starts with "q"
+        @Query("country")
+        countryCode: String = "us",
+
+        @Query("page")
+        pageNumber: Int = 1,
+
+        @Query("apiKey")
+        apiKey: String = API_KEY
+
+    //This function will return our news response
+    ) : Response<NewsResponse>
+
+    @GET("v2/top-everything")
+    suspend fun searchForNews(
+
+        //These queries are gotten from the full get request string that starts with "q"
+        @Query("q")
+        searchQuery: String,
+
+        @Query("page")
+        pageNumber: Int = 1,
+
+        @Query("apiKey")
+        apiKey: String = API_KEY
+
+        //This function will return our news response
+    ) : Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "60e4d2312dd54c51858ca31d781c62ec"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
