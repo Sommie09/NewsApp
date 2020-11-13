96e6e41ce0c299cc26c8caa1d8273ba94cdb9f40
Sommie09 <ceekay2a7@gmail.com>
2020-11-13 20:18:45 +0100

    Connected view models to fragments Set up custom network response class

---
+++ app/src/main/java/com/example/newsapp/repository/NewsRepository.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.repository
+
+import com.example.newsapp.database.ArticleDatabase
+import com.example.newsapp.model.Article
+
+class NewsRepository(val db : ArticleDatabase){
+
+}
